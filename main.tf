3547e2001019f4cd32aef8ab535b967b07931982
Sanderson de Paula <31490653+sandbardev@users.noreply.github.com>
2026-07-07 14:45:00 -0300

    Remove infrastructure (#2)

--- main.tf
+++ main.tf
@@ -12,12 +12,3 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "my-local-project"
-  }
-}
