47479cf55a639d93a96372d07b8e3e7967b18b18
Sanderson de Paula <31490653+sandbardev@users.noreply.github.com>
2026-07-07 14:39:56 -0300

    Add infrastructure info

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.micro"
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-#   tags = {
-#     Name = "my-local-project"
-#   }
-# }
+  tags = {
+    Name = "my-local-project"
+  }
+}
