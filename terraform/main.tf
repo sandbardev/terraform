254dcdd4d43e696aef76be6509c859d30ae1d5d2
Sanderson de Paula <31490653+sandbardev@users.noreply.github.com>
2026-07-18 21:37:02 -0300

    Create vpc, subnets & EKS cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,69 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = "t2.micro"
+# filter to fetch only default AZs
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
-#   tags = {
-#     Name = "terraform-placeholder"
-#   }
-# }
+# vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1" 
+
+  name = "main-vpc"
+
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  # config for using when setting up EKS cluster
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+# eks cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = "main-eks-cluster"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access_cidrs = ["179.218.21.185"] # my IP
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+}
