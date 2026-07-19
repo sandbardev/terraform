8bf63b916dd7b19c5b239e99e87b5b62b11ca954
Sanderson de Paula <31490653+sandbardev@users.noreply.github.com>
2026-07-18 22:06:01 -0300

    Remove all resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,148 +21,148 @@ data "aws_availability_zones" "available" {
   }
 }
 
-# GH Actions manually created, but permissions attached via IaC
-data "aws_iam_user" "github_actions" {
-  user_name = "github-actions"
-}
+# # GH Actions manually created, but permissions attached via IaC
+# data "aws_iam_user" "github_actions" {
+#   user_name = "github-actions"
+# }
 
-resource "aws_iam_policy" "github_actions_eks" {
-  name        = "github-actions-eks-policy"
-  description = "Permissões para GitHub Actions gerenciar EKS e IAM roles"
+# resource "aws_iam_policy" "github_actions_eks" {
+#   name        = "github-actions-eks-policy"
+#   description = "Permissões para GitHub Actions gerenciar EKS e IAM roles"
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          # Permissões IAM que faltaram
-          "iam:CreateRole",
-          "iam:DeleteRole",
-          "iam:GetRole",
-          "iam:UpdateRole",
-          "iam:AttachRolePolicy",
-          "iam:DetachRolePolicy",
-          "iam:PutRolePolicy",
-          "iam:DeleteRolePolicy",
-          "iam:ListRolePolicies",
-          "iam:ListAttachedRolePolicies",
-          "iam:PassRole",
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           # Permissões IAM que faltaram
+#           "iam:CreateRole",
+#           "iam:DeleteRole",
+#           "iam:GetRole",
+#           "iam:UpdateRole",
+#           "iam:AttachRolePolicy",
+#           "iam:DetachRolePolicy",
+#           "iam:PutRolePolicy",
+#           "iam:DeleteRolePolicy",
+#           "iam:ListRolePolicies",
+#           "iam:ListAttachedRolePolicies",
+#           "iam:PassRole",
 
-          # Permissões para EKS
-          "eks:CreateCluster",
-          "eks:DeleteCluster",
-          "eks:DescribeCluster",
-          "eks:UpdateClusterVersion",
-          "eks:CreateNodegroup",
-          "eks:DeleteNodegroup",
-          "eks:DescribeNodegroup",
-          "eks:UpdateNodegroupConfig",
+#           # Permissões para EKS
+#           "eks:CreateCluster",
+#           "eks:DeleteCluster",
+#           "eks:DescribeCluster",
+#           "eks:UpdateClusterVersion",
+#           "eks:CreateNodegroup",
+#           "eks:DeleteNodegroup",
+#           "eks:DescribeNodegroup",
+#           "eks:UpdateNodegroupConfig",
 
-          # Permissões para VPC e Subnets
-          "ec2:CreateVpc",
-          "ec2:DeleteVpc",
-          "ec2:CreateSubnet",
-          "ec2:DeleteSubnet",
-          "ec2:CreateInternetGateway",
-          "ec2:DeleteInternetGateway",
-          "ec2:CreateRouteTable",
-          "ec2:DeleteRouteTable",
-          "ec2:CreateRoute",
-          "ec2:DeleteRoute",
-          "ec2:AssociateRouteTable",
-          "ec2:DisassociateRouteTable",
-          "ec2:CreateSecurityGroup",
-          "ec2:DeleteSecurityGroup",
-          "ec2:AuthorizeSecurityGroupIngress",
-          "ec2:RevokeSecurityGroupIngress",
-          "ec2:Describe*",
-          "ec2:AllocateAddress",
-          "ec2:ReleaseAddress",
-          "ec2:CreateNatGateway",
-          "ec2:DeleteNatGateway"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
+#           # Permissões para VPC e Subnets
+#           "ec2:CreateVpc",
+#           "ec2:DeleteVpc",
+#           "ec2:CreateSubnet",
+#           "ec2:DeleteSubnet",
+#           "ec2:CreateInternetGateway",
+#           "ec2:DeleteInternetGateway",
+#           "ec2:CreateRouteTable",
+#           "ec2:DeleteRouteTable",
+#           "ec2:CreateRoute",
+#           "ec2:DeleteRoute",
+#           "ec2:AssociateRouteTable",
+#           "ec2:DisassociateRouteTable",
+#           "ec2:CreateSecurityGroup",
+#           "ec2:DeleteSecurityGroup",
+#           "ec2:AuthorizeSecurityGroupIngress",
+#           "ec2:RevokeSecurityGroupIngress",
+#           "ec2:Describe*",
+#           "ec2:AllocateAddress",
+#           "ec2:ReleaseAddress",
+#           "ec2:CreateNatGateway",
+#           "ec2:DeleteNatGateway"
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
 
-# 3. Anexar a política ao usuário
-resource "aws_iam_user_policy_attachment" "github_actions" {
-  user       = data.aws_iam_user.github_actions.user_name
-  policy_arn = aws_iam_policy.github_actions_eks.arn
-}
+# # 3. Anexar a política ao usuário
+# resource "aws_iam_user_policy_attachment" "github_actions" {
+#   user       = data.aws_iam_user.github_actions.user_name
+#   policy_arn = aws_iam_policy.github_actions_eks.arn
+# }
 
-# 4. Opcional: Adicionar política AWS gerenciada para EKS
-resource "aws_iam_user_policy_attachment" "github_actions_eks_managed" {
-  user       = data.aws_iam_user.github_actions.user_name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+# # 4. Opcional: Adicionar política AWS gerenciada para EKS
+# resource "aws_iam_user_policy_attachment" "github_actions_eks_managed" {
+#   user       = data.aws_iam_user.github_actions.user_name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# }
 
-resource "aws_iam_user_policy_attachment" "github_actions_vpc_managed" {
-  user       = data.aws_iam_user.github_actions.user_name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-}
-
-
-# vpc
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.8.1"
-
-  name = "main-vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  # config for using when setting up EKS cluster
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
-# eks cluster
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.5"
-
-  cluster_name    = "main-eks-cluster"
-  cluster_version = "1.29"
-
-  cluster_endpoint_public_access           = true
-  enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access_cidrs     = ["179.218.21.185"] # my IP
-  vpc_id                                   = module.vpc.vpc_id
-  subnet_ids                               = module.vpc.private_subnets
+# resource "aws_iam_user_policy_attachment" "github_actions_vpc_managed" {
+#   user       = data.aws_iam_user.github_actions.user_name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+# }
 
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+# # vpc
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.8.1"
 
-  }
+#   name = "main-vpc"
 
-  eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+#   cidr = "10.0.0.0/16"
+#   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-      instance_types = ["t3.small"]
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-      min_size     = 1
-      max_size     = 3
-      desired_size = 1
-    }
-  }
-}
+#   enable_nat_gateway = true
+#   single_nat_gateway = true
 
-# TODO: allow gh actions to create necessary roles
+#   # config for using when setting up EKS cluster
+#   public_subnet_tags = {
+#     "kubernetes.io/role/elb" = 1
+#   }
+
+#   private_subnet_tags = {
+#     "kubernetes.io/role/internal-elb" = 1
+#   }
+# }
+
+# # eks cluster
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.8.5"
+
+#   cluster_name    = "main-eks-cluster"
+#   cluster_version = "1.29"
+
+#   cluster_endpoint_public_access           = true
+#   enable_cluster_creator_admin_permissions = true
+#   cluster_endpoint_public_access_cidrs     = ["179.218.21.185"] # my IP
+#   vpc_id                                   = module.vpc.vpc_id
+#   subnet_ids                               = module.vpc.private_subnets
+
+
+#   eks_managed_node_group_defaults = {
+#     ami_type = "AL2_x86_64"
+
+#   }
+
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
+
+#       instance_types = ["t3.small"]
+
+#       min_size     = 1
+#       max_size     = 3
+#       desired_size = 1
+#     }
+#   }
+# }
+
+# # TODO: allow gh actions to create necessary roles
