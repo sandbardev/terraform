cf0c4bdc146aec3c174fe7cec62348d0c0b64fc7
Sanderson de Paula <31490653+sandbardev@users.noreply.github.com>
2026-07-18 22:04:35 -0300

    Allow existing resources & add permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -105,64 +105,64 @@ resource "aws_iam_user_policy_attachment" "github_actions_vpc_managed" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
 
-### Temporariamente desabilitados:
-# # vpc
-# module "vpc" {
-#   source  = "terraform-aws-modules/vpc/aws"
-#   version = "5.8.1"
 
-#   name = "main-vpc"
+# vpc
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
-#   cidr = "10.0.0.0/16"
-#   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  name = "main-vpc"
 
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-#   enable_nat_gateway = true
-#   single_nat_gateway = true
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
-#   # config for using when setting up EKS cluster
-#   public_subnet_tags = {
-#     "kubernetes.io/role/elb" = 1
-#   }
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
-#   private_subnet_tags = {
-#     "kubernetes.io/role/internal-elb" = 1
-#   }
-# }
+  # config for using when setting up EKS cluster
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
 
-# # eks cluster
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "20.8.5"
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
 
-#   cluster_name    = "main-eks-cluster"
-#   cluster_version = "1.29"
+# eks cluster
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
 
-#   cluster_endpoint_public_access           = true
-#   enable_cluster_creator_admin_permissions = true
-#   cluster_endpoint_public_access_cidrs     = ["179.218.21.185"] # my IP
-#   vpc_id                                   = module.vpc.vpc_id
-#   subnet_ids                               = module.vpc.private_subnets
+  cluster_name    = "main-eks-cluster"
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+  cluster_endpoint_public_access_cidrs     = ["179.218.21.185"] # my IP
+  vpc_id                                   = module.vpc.vpc_id
+  subnet_ids                               = module.vpc.private_subnets
 
 
-#   eks_managed_node_group_defaults = {
-#     ami_type = "AL2_x86_64"
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
 
-#   }
+  }
 
-#   eks_managed_node_groups = {
-#     one = {
-#       name = "node-group-1"
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
 
-#       instance_types = ["t3.small"]
+      instance_types = ["t3.small"]
 
-#       min_size     = 1
-#       max_size     = 3
-#       desired_size = 1
-#     }
-#   }
-# }
+      min_size     = 1
+      max_size     = 3
+      desired_size = 1
+    }
+  }
+}
 
-# # TODO: allow gh actions to create necessary roles
+# TODO: allow gh actions to create necessary roles
